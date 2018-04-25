c65a9b2ec6db1abad716a5a7495342adee7811fd
dhwajad <dhwajad@gmail.com>
2018-04-24 20:51:10 -0400

    Setting up Async

--- src/main/java/sleuth/webmvc/Frontend.java
+++ src/main/java/sleuth/webmvc/Frontend.java
@@ -1,36 +1,12 @@
 package sleuth.webmvc;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.Async;
-import org.springframework.scheduling.annotation.AsyncResult;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
+import org.springframework.context.annotation.ComponentScan;
 
 @EnableAutoConfiguration
-@RestController
-@CrossOrigin // So that javascript can be hosted elsewhere
+@ComponentScan(basePackageClasses = {FrontendController.class, BackendRepository.class})
 public class Frontend {
-
-  @Autowired BackendRepository backendRepository;
-
-  @RequestMapping("/")
-  public String callBackend() throws ExecutionException, InterruptedException {
-    //return restTemplate.getForObject("http://localhost:9000/api", String.class);
-    Future<String> result1 = backendRepository.callingBackendAsync();
-    Future<String> result2 = backendRepository.callingBackendAsync();
-    Future<String> result3 = backendRepository.callingBackendAsync();
-    return result1.get() + "<br />" + result2.get() + "<br />" + result3.get();
-  }
 
   public static void main(String[] args) {
     SpringApplication.run(Frontend.class,
---
+++ src/main/java/sleuth/webmvc/FrontendController.java
@@ -0,0 +1,27 @@
+package sleuth.webmvc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+@RestController
+@CrossOrigin // So that javascript can be hosted elsewhere
+public class FrontendController {
+
+    @Autowired
+    BackendRepository backendRepository;
+
+    @RequestMapping("/")
+    public String callBackend() throws ExecutionException, InterruptedException {
+        //return restTemplate.getForObject("http://localhost:9000/api", String.class);
+        Future<String> result1 = backendRepository.callingBackendAsync();
+        Future<String> result2 = backendRepository.callingBackendAsync();
+        Future<String> result3 = backendRepository.callingBackendAsync();
+        return result1.get() + "<br />" + result2.get() + "<br />" + result3.get();
+    }
+
+}
