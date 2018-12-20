101bf6b2d7eb3c1204f665dcbcb71ae1dcfc4b8b
Marcin Grzejszczak <marcin@grzejszczak.pl>
2018-12-20 11:08:43 +0100

    Removed Sleuth

--- src/main/java/sleuth/webmvc/frontend/FrontendAsyncConfigurerSupport.java
+++ src/main/java/sleuth/webmvc/frontend/FrontendAsyncConfigurerSupport.java
@@ -1,39 +1,31 @@
 package sleuth.webmvc.frontend;
 
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
+import java.util.concurrent.Executor;
+
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.AsyncConfigurerSupport;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
-import java.util.concurrent.Executor;
-
 @Configuration
 @EnableAsync
-public class FrontendAsyncConfigurerSupport {
+public class FrontendAsyncConfigurerSupport extends AsyncConfigurerSupport {
 
-    @Autowired
-    BeanFactory beanFactory;
+    public FrontendAsyncConfigurerSupport() {
+        System.out.println("Creation");
+    }
 
-    @Bean
-    public AsyncConfigurer asyncConfigurer() {
-        return new AsyncConfigurerSupport() {
-            @Override
-            public Executor getAsyncExecutor() {
-                ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-                executor.setCorePoolSize(10);
-                executor.setMaxPoolSize(10);
-                executor.setQueueCapacity(500);
-                executor.setThreadNamePrefix("AsyncExecutor-");
-                executor.initialize();
+    @Override
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(10);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(500);
+        executor.setThreadNamePrefix("AsyncExecutor-");
+        executor.initialize();
 
-                return executor;
-                //return new LazyTraceExecutor(beanFactory, executor);
-            }
-        };
+        return executor;
+        //return new LazyTraceExecutor(beanFactory, executor);
     }
 
 }
